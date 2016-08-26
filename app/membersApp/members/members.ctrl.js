bce760f9619d6b372e22c20b38acf8d996291a0d
Amanda Roscoe <aroscoe@corpu.com>
2016-08-26 11:46:17 -0400

    filter for only email, first_name, last_name with pagination and sorting. outstanding: last page number of showing issue exceeds length (showing 75-100 of 88)

--- app/membersApp/members/members.ctrl.js
+++ app/membersApp/members/members.ctrl.js
@@ -13,20 +13,26 @@ angular
 
   $urlRouterProvider.otherwise('/');
 })
-.controller('membersViewController', function($http, $scope, filterFilter){
+.controller('membersViewController', function($http, $scope, filterFilter, $filter){
 
   $scope.list = [];
-  $scope.pageSize = 5;
+  $scope.pageSize = 25;
+  $scope.sort = {       
+    sortingOrder : 'email',
+    reverse : false
+  };
   
   $scope.init = function () {
     $http.get('app/membersApp/members/data/MOCK_DATA.json').success(function (data) {
       $scope.list = data;
 
       $scope.$watch('search', function (term) {
-        // var obj = { first_name: term };
-
-        // $scope.filterList = filterFilter($scope.list, obj);
-        $scope.filterList = filterFilter($scope.list, term);
+        $scope.filterList= $scope.list.filter(function(row){
+          return (
+            angular.lowercase(row.email).indexOf(angular.lowercase($scope.search) || '') !== -1 ||
+            angular.lowercase(row.first_name).indexOf(angular.lowercase($scope.search) || '') !== -1 || 
+            angular.lowercase(row.last_name).indexOf(angular.lowercase($scope.search) || '') !== -1);
+        });
         $scope.currentPage = 1;
       });
     });
@@ -39,4 +45,41 @@ angular
     start = +start;
     return input.slice(start);
   };
+})
+.directive("customSort", function() {
+return {
+    restrict: 'A',
+    transclude: true,    
+    scope: {
+      order: '=',
+      sort: '='
+    },
+    template : 
+      ' <div ng-click="sort_by(order)" style="color: #555555; cursor: pointer;">'+
+      '    <span ng-transclude></span>'+
+      '    <i ng-class="selectedCls(order)"></i>'+
+      '</div>',
+    link: function(scope) {
+    // change sorting order
+    scope.sort_by = function(newSortingOrder) {   
+        var sort = scope.sort;
+        
+        if (sort.sortingOrder == newSortingOrder){
+            sort.reverse = !sort.reverse;
+        }                    
+
+        sort.sortingOrder = newSortingOrder;        
+    };
+    
+   
+    scope.selectedCls = function(column) {
+        if(column == scope.sort.sortingOrder){
+            return ('fa fa-chevron-' + ((scope.sort.reverse) ? 'down' : 'up'));
+        }
+        else{            
+            return'fa fa-sort' 
+        } 
+    };      
+  }// end link
+}
 });
